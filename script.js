19374cce98f366daa1309b8f4b67a78dab6987d2
Kirill1962 <82878512+Kirill1962@users.noreply.github.com>
2021-04-29 14:05:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var str1 = prompt('Введите ваше имя:');
+var str2 = prompt('Введите вашу фамилию:');
+var num = +prompt('Введите год вашего рождения:');
+var year = 2021;
+console.log(' я ' + (  str1  ) + (  str2  ) + ' мне ' + ( num - year ) + ' лет ')
